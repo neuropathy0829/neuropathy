960ece518de8202f35a42a34af5ad63901bc852d
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2023-04-01 19:27:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var button = document.getElementById("scare-button");
-var audio = new Audio("scary-sound.mp3");
+var audio = new Audio("1.mp3");
+var img = document.createElement('img');
+var container = document.getElementById('image-container');
+img.src = '1.jpg';
+img.alt = '圖片';
 
 button.onclick = function() {
     alert("你被嚇到了吧！");
     alert("哈哈");
     audio.play();
+    container.appendChild(img);
 };
