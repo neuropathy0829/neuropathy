cef5296c8e6d5b4985d83b1fc248b0ea907a7556
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2023-04-01 18:50:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var button = document.getElementById("scare-button");
+var audio = new Audio("1.mp3");
+
 button.onclick = function() {
     alert("你被嚇到了吧！");
     alert("哈哈");
+    audio.play();
 };
