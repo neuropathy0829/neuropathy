a454c657fb81b0bef046563f578404d76a0d1490
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2023-07-26 22:11:58 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,37 @@
-//禁用右鍵
-document.addEventListener('contextmenu', function (event) {
-  event.preventDefault();
-}, true);
-//禁用F12、Ctrl+Shift+I、Ctrl+Shift+J、Ctrl+U
-document.onkeydown = function (e) {
-  if (event.keyCode == 123) {
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-    return false;
-  }
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-    return false;
-  }
+// //禁用右鍵
+// document.addEventListener('contextmenu', function (event) {
+//   event.preventDefault();
+// }, true);
+// //禁用F12、Ctrl+Shift+I、Ctrl+Shift+J、Ctrl+U
+// document.onkeydown = function (e) {
+//   if (event.keyCode == 123) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//     return false;
+//   }
+// }
+//18歲彈窗
+document.addEventListener('DOMContentLoaded', function() {
+  showOverlay();
+});
+
+function showOverlay() {
+  document.getElementById('overlay').style.display = 'flex';
+  document.body.style.overflow = 'hidden'; // 隱藏網頁滾動
+}
+
+function closeOverlay() {
+  document.getElementById('overlay').style.display = 'none';
+  document.body.style.overflow = 'auto'; // 恢復網頁滾動
+}
+
+function redirectToOtherSite() {
+  window.location.href = 'https://reurl.cc/ZWgMWg'; // 替換成您想要跳轉的其他網站URL
 }
